a3bef45fac7048945035bf7276612de9aaefeeea
qiuping01 <2187718660@qq.com>
2026-02-24 20:46:25 +0800

    OJ判题后端初始化

---
+++ src/main/java/com/ping/oj/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.ping.oj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ping.oj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ **/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
