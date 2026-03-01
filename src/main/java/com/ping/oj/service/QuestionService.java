d7a153271ae8798f04657673b02d597962ed27df
qiuping01 <2187718660@qq.com>
2026-03-01 20:41:34 +0800

    feat(question): 题目服务开发

--- src/main/java/com/ping/oj/service/QuestionService.java
+++ src/main/java/com/ping/oj/service/QuestionService.java
@@ -1,13 +1,54 @@
 package com.ping.oj.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ping.oj.model.dto.post.PostQueryRequest;
+import com.ping.oj.model.dto.question.QuestionQueryRequest;
+import com.ping.oj.model.entity.Post;
 import com.ping.oj.model.entity.Question;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.ping.oj.model.vo.PostVO;
+import com.ping.oj.model.vo.QuestionVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
-* @author 21877
-* @description 针对表【question(题目)】的数据库操作Service
-* @createDate 2026-03-02 00:09:00
-*/
+ * 题目服务
+ */
 public interface QuestionService extends IService<Question> {
+
+    /**
+     * 校验题目是否合法
+     *
+     * @param question 题目实体
+     * @param add      是否为新增题目
+     */
+    void validQuestion(Question question, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionQueryRequest 题目查询请求
+     * @return 题目查询条件
+     */
+    QueryWrapper<Question> getQueryWrapper(QuestionQueryRequest questionQueryRequest);
+
+    /**
+     * 获取题目封装
+     *
+     * @param question 题目实体
+     * @param request  Http请求
+     * @return 题目封装对象
+     */
+    QuestionVO getQuestionVO(Question question, HttpServletRequest request);
+
+    /**
+     * 分页获取题目封装
+     *
+     * @param questionPage 题目分页
+     * @param request      Http请求
+     * @return 题目封装分页
+     */
+    Page<QuestionVO> getQuestionVOPage(Page<Question> questionPage, HttpServletRequest request);
 
 }
--- src/main/java/com/ping/oj/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/ping/oj/service/impl/QuestionServiceImpl.java
@@ -1,20 +1,173 @@
 package com.ping.oj.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.ping.oj.model.entity.Question;
-import com.ping.oj.service.QuestionService;
+import com.ping.oj.common.ErrorCode;
+import com.ping.oj.constant.CommonConstant;
+import com.ping.oj.exception.BusinessException;
+import com.ping.oj.exception.ThrowUtils;
 import com.ping.oj.mapper.QuestionMapper;
+import com.ping.oj.model.dto.question.QuestionQueryRequest;
+import com.ping.oj.model.entity.Question;
+import com.ping.oj.model.entity.User;
+import com.ping.oj.model.vo.QuestionVO;
+import com.ping.oj.model.vo.UserVO;
+import com.ping.oj.service.QuestionService;
+import com.ping.oj.service.UserService;
+import com.ping.oj.utils.SqlUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
-* @author 21877
-* @description 针对表【question(题目)】的数据库操作Service实现
-* @createDate 2026-03-02 00:09:00
-*/
+ * 题目服务实现
+ */
 @Service
 public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question>
-    implements QuestionService{
+        implements QuestionService {
 
+    @Resource
+    private UserService userService;
+
+    /**
+     * 校验题目是否合法
+     *
+     * @param question 题目实体
+     * @param add      是否为新增题目
+     */
+    @Override
+    public void validQuestion(Question question, boolean add) {
+        if (question == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        String title = question.getTitle();
+        String content = question.getContent();
+        String tags = question.getTags();
+        String answer = question.getAnswer();
+        String judgeCase = question.getJudgeCase();
+        String judgeConfig = question.getJudgeConfig();
+        // 创建时，参数不能为空
+        if (add) {
+            ThrowUtils.throwIf(StringUtils.isAnyBlank(title, content, tags), ErrorCode.PARAMS_ERROR);
+        }
+        // 有参数则校验
+        if (StringUtils.isNotBlank(title) && title.length() > 80) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "标题过长");
+        }
+        if (StringUtils.isNotBlank(content) && content.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "内容过长");
+        }
+        if (StringUtils.isNotBlank(answer) && answer.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "答案过长");
+        }
+        if (StringUtils.isNotBlank(judgeCase) && judgeCase.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "判题用例过长");
+        }
+        if (StringUtils.isNotBlank(judgeConfig) && judgeCase.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "判题配置过长");
+        }
+    }
+
+    /**
+     * 获取查询包装类（根据前端传入的查询请求对象，将查询条件转换为 MyBatis Plus 支持的 QueryWrapper）
+     *
+     * @param questionQueryRequest 题目查询请求
+     * @return 查询条件包装类
+     */
+    @Override
+    public QueryWrapper<Question> getQueryWrapper(QuestionQueryRequest questionQueryRequest) {
+        QueryWrapper<Question> queryWrapper = new QueryWrapper<>();
+        if (questionQueryRequest == null) {
+            return queryWrapper;
+        }
+        Long id = questionQueryRequest.getId();
+        String title = questionQueryRequest.getTitle();
+        String content = questionQueryRequest.getContent();
+        List<String> tagList = questionQueryRequest.getTags();
+        Long userId = questionQueryRequest.getUserId();
+        String sortField = questionQueryRequest.getSortField();
+        String sortOrder = questionQueryRequest.getSortOrder();
+        // 拼接查询条件
+        queryWrapper.like(StringUtils.isNotBlank(title), "title", title);
+        queryWrapper.like(StringUtils.isNotBlank(content), "content", content);
+        if (CollUtil.isNotEmpty(tagList)) {
+            for (String tag : tagList) {
+                queryWrapper.like("tags", "\"" + tag + "\"");
+            }
+        }
+        queryWrapper.eq(ObjectUtils.isNotEmpty(id), "id", id);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+        // 只查询没有删除的题目
+        queryWrapper.eq("isDelete", false);
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField), sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+
+    /**
+     * 获取题目封装
+     *
+     * @param question 题目实体
+     * @param request  Http请求
+     * @return 题目封装对象
+     */
+    @Override
+    public QuestionVO getQuestionVO(Question question, HttpServletRequest request) {
+        QuestionVO questionVO = QuestionVO.objToVo(question);
+        Long userId = question.getUserId();
+        User user = null;
+        if (userId != null && userId > 0) {
+            user = userService.getById(userId);
+        }
+        UserVO userVO = userService.getUserVO(user);
+        questionVO.setUserVO(userVO);
+        return questionVO;
+    }
+
+    /**
+     * 分页获取题目封装
+     *
+     * @param questionPage 题目分页
+     * @param request      Http请求
+     * @return 题目封装分页
+     */
+    @Override
+    public Page<QuestionVO> getQuestionVOPage(Page<Question> questionPage, HttpServletRequest request) {
+        List<Question> questionList = questionPage.getRecords();
+        Page<QuestionVO> questionVOPage = new Page<>(questionPage.getCurrent(), questionPage.getSize(), questionPage.getTotal());
+        if (CollectionUtils.isEmpty(questionList)) {
+            return questionVOPage;
+        }
+        // 1. 关联查询用户信息
+        Set<Long> userIdSet = questionList.stream()
+                .map(Question::getUserId)
+                .collect(Collectors.toSet());
+        Map<Long, List<User>> userIdUserListMap = userService.listByIds(userIdSet).stream()
+                .collect(Collectors.groupingBy(User::getId));
+        // 2. 填充信息
+        List<QuestionVO> questionVOList = questionList.stream()
+                .map(question -> {
+                    QuestionVO questionVO = QuestionVO.objToVo(question);
+                    Long userId = question.getUserId();
+                    User user = null;
+                    if (userIdUserListMap.containsKey(userId)) {
+                        user = userIdUserListMap.get(userId).get(0);
+                    }
+                    questionVO.setUserVO(userService.getUserVO(user));
+                    return questionVO;
+                })
+                .collect(Collectors.toList());
+        questionVOPage.setRecords(questionVOList);
+        return questionVOPage;
+    }
 }
 
 
