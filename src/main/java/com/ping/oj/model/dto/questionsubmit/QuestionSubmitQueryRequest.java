e96b2f40295a50052cbcf3644a3e716974231f75
qiuping01 <2187718660@qq.com>
2026-03-02 20:00:54 +0800

    feat(question-submit): 题目提交数据模型设计

---
+++ src/main/java/com/ping/oj/model/dto/questionsubmit/QuestionSubmitQueryRequest.java
@@ -0,0 +1,34 @@
+package com.ping.oj.model.dto.questionsubmit;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 题目提交查询请求
+ **/
+@Data
+public class QuestionSubmitQueryRequest implements Serializable {
+
+    /**
+     * 编程语言
+     */
+    private String language;
+
+    /**
+     * 判题状态（0 - 待判题、1 - 判题中、2 - 成功、3 - 失败）
+     */
+    private Integer status;
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
