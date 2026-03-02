f6710395d98e4087e4ed5c4bec7af793eb9d95eb
qiuping01 <2187718660@qq.com>
2026-03-02 20:15:18 +0800

    fix(question-submit): 题目提交查询请求继承分页请求

--- src/main/java/com/ping/oj/model/dto/questionsubmit/QuestionSubmitQueryRequest.java
+++ src/main/java/com/ping/oj/model/dto/questionsubmit/QuestionSubmitQueryRequest.java
@@ -1,14 +1,17 @@
 package com.ping.oj.model.dto.questionsubmit;
 
+import com.ping.oj.common.PageRequest;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
 /**
  * 题目提交查询请求
  **/
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class QuestionSubmitQueryRequest implements Serializable {
+public class QuestionSubmitQueryRequest extends PageRequest implements Serializable {
 
     /**
      * 编程语言
