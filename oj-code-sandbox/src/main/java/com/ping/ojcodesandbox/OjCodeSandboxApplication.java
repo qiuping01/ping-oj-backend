d18d7407663366dca2723f382a58b54622f47dac
qiuping01 <2187718660@qq.com>
2026-03-04 15:54:53 +0800

    feat(oj-code-sandbox): 初始化代码沙箱模块，添加基础配置和健康检查接口

---
+++ oj-code-sandbox/src/main/java/com/ping/ojcodesandbox/OjCodeSandboxApplication.java
@@ -0,0 +1,12 @@
+package com.ping.ojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjCodeSandboxApplication.class, args);
+    }
+}
