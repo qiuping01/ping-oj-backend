1a785acd94f3912167902445fbd1ef154e288c65
qiuping01 <2187718660@qq.com>
2026-03-10 18:44:20 +0800

    feat(controller): 定义请求头和秘钥

--- oj-code-sandbox/src/main/java/com/ping/ojcodesandbox/controller/MainController.java
+++ oj-code-sandbox/src/main/java/com/ping/ojcodesandbox/controller/MainController.java
@@ -10,12 +10,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 主接口
  */
 @RestController("/")
 public class MainController {
+
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
 
     @Resource
     private JavaNativeCodeSandbox javaNativeCodeSandbox;
@@ -32,7 +39,14 @@ public class MainController {
      * @return
      */
     @PostMapping("executeCode")
-    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest){
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest,
+                                           HttpServletRequest request, HttpServletResponse response) {
+        // 基本认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
         if (executeCodeRequest == null) {
             throw new BusinessException("参数不能为空");
         }
