3f51aaea092ffa0ee5bc60892c4ad3282a9da92b
qiuping01 <2187718660@qq.com>
2026-03-10 17:35:07 +0800

    feat(controller): 添加主接口，先默认使用原生代码沙箱

--- oj-code-sandbox/src/main/java/com/ping/ojcodesandbox/JavaNativeCodeSandbox.java
+++ oj-code-sandbox/src/main/java/com/ping/ojcodesandbox/JavaNativeCodeSandbox.java
@@ -2,10 +2,12 @@ package com.ping.ojcodesandbox;
 
 import com.ping.ojcodesandbox.model.ExecuteCodeRequest;
 import com.ping.ojcodesandbox.model.ExecuteCodeResponse;
+import org.springframework.stereotype.Component;
 
 /**
  * Java 本地代码沙箱
  */
+@Component
 public class JavaNativeCodeSandbox extends JavaCodeSandboxTemplate {
 
     @Override
--- oj-code-sandbox/src/main/java/com/ping/ojcodesandbox/controller/MainController.java
+++ oj-code-sandbox/src/main/java/com/ping/ojcodesandbox/controller/MainController.java
@@ -1,7 +1,15 @@
 package com.ping.ojcodesandbox.controller;
 
+import com.ping.ojcodesandbox.JavaNativeCodeSandbox;
+import com.ping.ojcodesandbox.exception.BusinessException;
+import com.ping.ojcodesandbox.model.ExecuteCodeRequest;
+import com.ping.ojcodesandbox.model.ExecuteCodeResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * 主接口
@@ -9,8 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/")
 public class MainController {
 
+    @Resource
+    private JavaNativeCodeSandbox javaNativeCodeSandbox;
+
     @GetMapping("/health")
     public String healthCheck() {
         return "Hello ";
+    }
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    @PostMapping("executeCode")
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest){
+        if (executeCodeRequest == null) {
+            throw new BusinessException("参数不能为空");
+        }
+        return javaNativeCodeSandbox.executeCode(executeCodeRequest);
     }
 }
